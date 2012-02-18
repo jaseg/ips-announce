d6de85e2cf6ca02122d76228eb3366bb532f2835
Martin Holzhauer <martin@holzhauer.eu>
2012-02-18 21:18:23 +0100

    added blinky blink
    added svg map
    added svg map js magic

---
+++ sinatra/static/js/mapenhancer.js
@@ -0,0 +1,35 @@
+
+enhanceMap = function (doc) {
+
+    var roomHash = {};
+
+
+    $(doc).find('#rooms').find('g').each(function(i,e){
+        var id = $('tspan:contains("###")',e).attr('id');
+        var name = n= $('tspan',e).not(':contains("###")').text();
+
+        roomHash[ name ] = id;
+    });
+
+    console.log(roomHash);
+
+    function refreshSVGMap() {
+        $.each(roomHash, function (name, id) {
+            console.log(name, id);
+            $.getJSON('/users/by-room/'+ encodeURIComponent(name) , function(data){
+                console.log(name, id, data);
+                if( data ) {
+                    $(doc).find('#'+id).text( data.length + ' hacker'+(data.length==1?'':'s'));
+                } else {
+                    $(doc).find('#'+id).text("NIL");
+                }
+            } );
+        });
+    }
+
+    refreshSVGMap();
+
+    return {
+        'refresh' : refreshSVGMap
+    }
+}
